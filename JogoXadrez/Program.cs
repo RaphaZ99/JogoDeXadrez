f9408aba9e24ec75f57ba6b6164b6d705bd1786a
Raphael Mota <raphael.mota14@hotmail.com>
2019-09-16 14:49:05 -0300

    Criando a classe Posição

---
+++ JogoXadrez/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tabuleiro;
+
+namespace JogoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicção " + p);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ JogoXadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+
+
+    //classe pegando linha e posição da matriz
+    class Posicao
+    {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+
+
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+
+
+    }
+}
