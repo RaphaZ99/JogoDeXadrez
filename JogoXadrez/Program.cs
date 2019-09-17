c3324ee95ecc74216bdcf42b6482a3870987a6a2
Raphael Mota <raphael.mota14@hotmail.com>
2019-09-17 17:29:17 -0300

    Criando a classe Posicao Xadrez

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -7,21 +7,15 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+           
 
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 0));
+            Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
-            } catch(TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
+            Console.WriteLine(pos.toPosicao());
 
             }
         }
     }
-}
+
---
+++ JogoXadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,41 @@
+﻿using tabuleiro;
+namespace xadrez
+    
+{
+    class PosicaoXadrez
+    {
+        
+        public char coluna { get; set; }
+        
+            public int linha { get; set; }
+
+        //construtor da classe
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+
+        }
+
+
+        //metodo para trasnforma a posicao do string dado pelo usuario
+        //Em uma posição na matriz
+        //A e um numero inteiro coluna - a = 0, se for B
+        //B - A = 1, e assim por diante
+        public  Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+
+        }
+
+        //retornando os valores dados pelo usuario
+        //transforma o objeto em string
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+
+  
+
+}
