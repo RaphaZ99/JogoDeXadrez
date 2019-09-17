37a17909238b43710cc9e6fe269366be53172eb4
Raphael Mota <raphael.mota14@hotmail.com>
2019-09-17 17:10:37 -0300

    Tratando erros com excecao personalizada

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -7,16 +7,21 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 0));
 
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab,Cor.Branca), new Posicao(2, 4));
+                Tela.imprimirTabuleiro(tab);
+            } catch(TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
 
-            Tela.imprimirTabuleiro(tab);
-
+            }
         }
     }
 }
--- JogoXadrez/tabuleiro/Tabuleiro.cs
+++ JogoXadrez/tabuleiro/Tabuleiro.cs
@@ -18,6 +18,12 @@ namespace tabuleiro
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+        //criando sobrecarga para Peca recebendo uma Pos
+        public Peca peca(Posicao pos)
+        {
+
+            return pecas[pos.linha, pos.coluna];
+        }
 
         //metodo para retornar uma peça
         //Em forma de matriz
@@ -27,11 +33,47 @@ namespace tabuleiro
 
             return pecas[linha, coluna];
         }
+
+
+        //metodo para verificar se exixti uma peça em uma posicao do xadrez
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+
+        }
         //metodo para colocar  a Peca no xadrez
         public void colocarPeca(Peca p, Posicao pos)
         {
+            //Verificando se existe uma peça na posição Determinada
+            if(existePeca(pos)){
+
+                throw new TabuleiroException("Ja existe uma peça nessa Posição");
+            }
+
+
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        //metodo para testar se o Pos(posicão da peça)  é valida
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+
+            }
+            return true;
+        }
+
+        //metodo para verifica se uma posição e valida, caso não e lancado uma exceção
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Invalida");
+            }
+
         }
     }
 }
