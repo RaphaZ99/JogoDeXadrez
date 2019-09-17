c128a1cb78c25505873906d1c7830a5eab661d6e
Raphael Mota <raphael.mota14@hotmail.com>
2019-09-17 17:45:28 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -7,15 +7,26 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-           
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 5));
 
-            Console.WriteLine(pos.toPosicao());
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
+
+                Tela.imprimirTabuleiro(tab);
+            } catch(TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
 
             }
         }
     }
-
+}
--- JogoXadrez/Tela.cs
+++ JogoXadrez/Tela.cs
@@ -9,9 +9,11 @@ namespace JogoXadrez
     {
         //metodo para imprimir o tabuleiro
         public static void imprimirTabuleiro(Tabuleiro tab)
-        {
+        {     // numero que fica ao lado que vao de 1 a 8 
             for(int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
 
                 for(int j = 0; j < tab.colunas; j++)
                 {
@@ -20,17 +22,40 @@ namespace JogoXadrez
                         Console.Write("- ");
                     }else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i,j));
+                        Console.Write(" ");
                     }
                     
                 }
 
                 Console.WriteLine();
             }
+            //letras para se fazer as jogadas
+            Console.WriteLine("  a b c d e f g h");
 
 
         }
 
+        public static void imprimirPeca (Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                //cor padrao das letras
+                ConsoleColor corPadrao = Console.ForegroundColor;
+                //alterando as cores pra amarelo caso seja outro tipo de peca(peca adversaria)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+
+                //voltando a cor padrao das pecas
+                Console.ForegroundColor = corPadrao;
+
+            }
+
+        }
 
 
 
